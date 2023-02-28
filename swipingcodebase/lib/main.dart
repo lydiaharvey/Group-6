f4937169461d42629055fdbfb6642f142a350e2f
Lydia <lydiaharvey24@gmail.com>
2023-02-28 13:58:37 +0000

    update
    Signed-off-by: Lydia <lydiaharvey24@gmail.com>

---
+++ swipingcodebase/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [Align(alignment: Alignment(0,0),
+        child: Container(
+          height: MediaQuery.of(context).size.height*0.5,
+        width: MediaQuery.of(context).size.width*0.75,
+        color: Colors.green,))]
+        ,
+      )
+
+
+     // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
