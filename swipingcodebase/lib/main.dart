e060462ccf64ceb5fba9bd96503947da64a06b00
Lydia <lydiaharvey24@gmail.com>
2023-02-28 14:38:47 +0000

    Added app bar at the bottom with working button icons
    Signed-off-by: Lydia <lydiaharvey24@gmail.com>

--- swipingcodebase/lib/main.dart
+++ swipingcodebase/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  //var green = Color(sadasjk)
   runApp(const MyApp());
 }
 
@@ -31,20 +32,71 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Stack(
-        children: [Align(alignment: Alignment(0,0),
+        body: Stack(children: [
+      Align(
+          alignment: Alignment(0, -0.5),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.75,
+            color: Colors.green,
+          )),
+      navigationBar()
+    ])
+
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
+
+class navigationBar extends StatefulWidget {
+  const navigationBar({Key? key}) : super(key: key);
+
+  @override
+  _navigationBarState createState() => _navigationBarState();
+}
+
+class _navigationBarState extends State<navigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.bottomCenter,
         child: Container(
-          height: MediaQuery.of(context).size.height*0.5,
-        width: MediaQuery.of(context).size.width*0.75,
-        color: Colors.green,))]
-        ,
-      )
-
-
-     // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          height: MediaQuery.of(context).size.height * 0.10,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.green,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  padding: EdgeInsets.zero,
+                  style: IconButton.styleFrom(
+                      fixedSize: Size(MediaQuery.of(context).size.height * 0.1,
+                          MediaQuery.of(context).size.height * 0.1)),
+                  onPressed: () {},
+                  icon: Icon(color: Colors.white, Icons.home,size: 50)),
+              IconButton(
+                  padding: EdgeInsets.zero,
+                  style: IconButton.styleFrom(
+                      fixedSize: Size(MediaQuery.of(context).size.height * 0.1,
+                          MediaQuery.of(context).size.height * 0.1)),
+                  onPressed: () {},
+                  icon: Icon(color: Colors.white, Icons.emoji_events,size: 50)),
+              IconButton(
+                  padding: EdgeInsets.zero,
+                  style: IconButton.styleFrom(
+                      fixedSize: Size(MediaQuery.of(context).size.height * 0.1,
+                          MediaQuery.of(context).size.height * 0.1)),
+                  onPressed: () {},
+                  icon: Icon(color: Colors.white, Icons.shopping_bag,size: 50)),
+              IconButton(
+                  padding: EdgeInsets.zero,
+                  style: IconButton.styleFrom(
+                      fixedSize: Size(MediaQuery.of(context).size.height * 0.1,
+                          MediaQuery.of(context).size.height * 0.1)),
+                  onPressed: () {},
+                  icon: Icon(color: Colors.white, Icons.settings,size: 50,))
+            ],
+          ),
+        ));
   }
 }
