afa5a824283498bde43a4b81fdb6bef2538a1fa9
Sarah Higzi <91472896+sarahhigzi@users.noreply.github.com>
2023-03-09 13:54:48 +0000

    Create homepage.dart

---
+++ homepage.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'navigation.dart';
+import 'main.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GUESS IT'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const QuizPage('Easy A Quiz')),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.lightGreen,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              minimumSize: const Size(100.0, 40.0),
+            ),
+            child: const Text('Easy A'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const QuizPage('Easy B Quiz')),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              minimumSize: const Size(100.0, 40.0),
+            ),
+            child: const Text('Easy B'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const QuizPage('Medium A Quiz')),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orangeAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              minimumSize: const Size(100.0, 40.0),
+            ),
+            child: const Text('Medium A'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const QuizPage('Medium B Quiz')),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.orange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              minimumSize: const Size(100.0, 40.0),
+            ),
+            child: const Text('Medium B'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const QuizPage('Hard A Quiz')),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.redAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              minimumSize: const Size(100.0, 40.0),
+            ),
+            child: const Text('Hard A'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const QuizPage('Hard B Quiz')),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              minimumSize: const Size(100.0, 40.0),
+            ),
+            child: const Text('Hard B'),
+          ),
+        ],
+      ),
+    );
+  }
+}
