5400164768188ca3c24072f91b1d2f54a21337b7
Sarah Higzi <91472896+sarahhigzi@users.noreply.github.com>
2023-03-16 18:35:42 +0000

    Update and rename quiz.dart to difficulty_page.dart

---
+++ difficulty_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+import 'navigation.dart';
+import 'main.dart';
+
+
+class QuizPage extends StatelessWidget {
+  final String title;
+
+  const QuizPage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      // rest of your code
+    );
+  }
+}
+
+class DifficultyPage extends StatelessWidget {
+  final String title;
+
+  const DifficultyPage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 64.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 16.0,
+          crossAxisSpacing: 16.0,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SizedBox(child: QuizPage(title: 'Easy A Quiz'))),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightGreen,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(0, 64.0),
+              ),
+              child: const Text('Easy A'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SizedBox(child: QuizPage(title: 'Easy B Quiz'))),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(0, 64.0),
+              ),
+              child: const Text('Easy B'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SizedBox(child: QuizPage(title: 'Medium A Quiz'))),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orangeAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(0, 64.0),
+              ),
+              child: const Text('Medium A'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SizedBox(child: QuizPage(title: 'Medium B Quiz'))),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(0, 64.0),
+              ),
+              child: const Text('Medium B'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SizedBox(child: QuizPage(title: 'Hard A Quiz'))),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(0, 64.0),
+              ),
+              child: const Text('Hard A'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SizedBox(child: QuizPage(title: 'Hard B Quiz'))),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(0, 64.0),
+              ),
+              child: const Text('Hard B'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
