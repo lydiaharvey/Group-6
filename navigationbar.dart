81f2f56fd61a5b3e417be5cdba6c00c4d87443a0
Sarah Higzi <91472896+sarahhigzi@users.noreply.github.com>
2023-03-16 18:43:01 +0000

    Update navigationbar.dart

--- navigationbar.dart
+++ navigationbar.dart
@@ -9,7 +9,7 @@ class NavigationBar extends StatefulWidget {
   }
 }
 
-class _navigationBarState extends State<navigationBar> {
+class _NavigationBarState extends State<NavigationBar> {
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -54,3 +54,87 @@ class _navigationBarState extends State<navigationBar> {
         ));
   }
 }
+
+/// possible change 
+
+/*
+
+class _NavigationBarState extends State<NavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.10,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.blueAccent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              padding: EdgeInsets.zero,
+              style: IconButton.styleFrom(
+                fixedSize: Size(
+                  MediaQuery.of(context).size.height * 0.1,
+                  MediaQuery.of(context).size.height * 0.1,
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.home,
+                color: Colors.blueAccent,
+                size: 50,
+              ),
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              style: IconButton.styleFrom(
+                fixedSize: Size(
+                  MediaQuery.of(context).size.height * 0.1,
+                  MediaQuery.of(context).size.height * 0.1,
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.emoji_events,
+                color: Colors.blueAccent,
+                size: 50,
+              ),
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              style: IconButton.styleFrom(
+                fixedSize: Size(
+                  MediaQuery.of(context).size.height * 0.1,
+                  MediaQuery.of(context).size.height * 0.1,
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.shopping_bag,
+                color: Colors.blueAccent,
+                size: 50,
+              ),
+            ),
+            IconButton(
+              padding: EdgeInsets.zero,
+              style: IconButton.styleFrom(
+                fixedSize: Size(
+                  MediaQuery.of(context).size.height * 0.1,
+                  MediaQuery.of(context).size.height * 0.1,
+                ),
+              ),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.settings,
+                color: Colors.blueAccent,
+                size: 50,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
